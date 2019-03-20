c05f5c8d77b80525d255c2aae29752ed8a053aba
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-20 10:01:23 -0400

    Completed all till the toString method

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,16 +1,37 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+
+import java.util.List;
 
 public class GroceryReporter {
     private final String originalFileText;
+    private ItemParser itemParser;
 
     public GroceryReporter(String jerksonFileName) {
+        this.itemParser = new ItemParser();
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
 
     @Override
     public String toString() {
-        return null;
+        List<Item> itemList = itemParser.parseItemList(originalFileText);
+
+
+        String newString = itemList.toString();
+        return newString;
+    }
+
+    public static void main(String[] args) {
+        GroceryReporter gr = new GroceryReporter("RawInput.JerkSon");
+        ItemParser ip = new ItemParser();
+        String list = gr.toString();
+
+        List<Item> itemList = ip.parseItemList(list);
+
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,7 +2,7 @@ package io.zipcoder;
 
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,62 +10,36 @@ import java.util.regex.Pattern;
 public class ItemParser {
     private Pattern pattern;
     private Matcher matcher;
+
     public List<Item> parseItemList(String valueToParse) {
-        return null;
+        List<Item> returnList = new ArrayList<>();
+        String[] stringArray = valueToParse.split("##");
+        for(String string : stringArray){
+            try {returnList.add(parseSingleItem(string));}
+            catch (ItemParseException e ){}
+        }
+        return returnList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
-        String name = getValue("name",singleItem);
-        Double price = Double.valueOf(getValue("price",singleItem));
-        String type;
-        String expiration;
+        String replace = singleItem.toLowerCase();
 
-        //String replace = singleItem.replaceAll("##", "");
-        //String[] split = replace.split("[:@^*%;]");
+        String name = getValue("(NAME)([:@^*%]?)(\\w+)", replace);
+        Double price = Double.valueOf(getValue("(price?)([:@^*%]?)(\\d+\\.\\d{1,2})", replace));
+        String type = getValue("(type)([:@^*%]?)(\\w+)", replace);
+        String expiration = getValue("(expiRatiOn)([:@^*%]?)(\\d/.*)", replace);
 
-//        String namePattern = "(name?)([:@^*%;]?)(\\w+)";
-//        String pricePattern = "(name?)([:@^*%;]?)(\\w+)";
-//        pattern = Pattern.compile(namePattern, Pattern.CASE_INSENSITIVE);
-//        matcher = pattern.matcher(singleItem);
-//
-//        while(matcher.find()){
-//            name = matcher.group(3);
-//        }
-
-
-        return new Item(name,price,"", "");
+        return new Item(name, price, type, expiration);
     }
 
-    private String getValue(String string, String sentence){
-        String result = "";
-        String somePattern = "("+string+"?)([:@^*%;]?)(\\w+)";
-        pattern = Pattern.compile(somePattern, Pattern.CASE_INSENSITIVE);
+    private String getValue(String string, String sentence) throws ItemParseException {
+        pattern = Pattern.compile(string, Pattern.CASE_INSENSITIVE);
         matcher = pattern.matcher(sentence);
-
-        while(matcher.find()){
-            result = matcher.group(3);
+        if (matcher.find()) {
+            return matcher.group(3);
+        } else {
+            throw new ItemParseException();
         }
-        return result;
-    }
-
-    private Double getDoubleValue(String string, String sentence){
-        String result = "";
-        String somePattern = "("+string+"?)([:@^*%;]?)(\\w+)";
-        pattern = Pattern.compile(somePattern, Pattern.CASE_INSENSITIVE);
-        matcher = pattern.matcher(sentence);
-
-        while(matcher.find()){
-            result = matcher.group(3);
-        }
-        return 0.0;
-    }
-
-
-    public static void main(String[] args) throws ItemParseException{
-        ItemParser ip = new ItemParser();
-        //ip.parseSingleItem("naMe:apPles;prIce:0.25;type:Food;expiration:1/23/2016##naMe:apPles;pRice:0.23;type:Food;expiration:5/02/2016##NAMe:BrEAD;price:1.23;type:Food;expiration:1/25/2016##naMe:;price:3.23;type:Food^expiration:1/04/2016##");
-
-        Item item = ip.parseSingleItem("naMe@SOMETHING;price:3.23;type:Food^expiration:1/04/2016##");
-        System.out.println(item.toString());
     }
 }
+
